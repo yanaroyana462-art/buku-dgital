1c9702aad3c605f281291908d3cefcff1712e77d
yanaroyana462-art <yanaroyana462@gmail.com>
2026-02-03 04:21:04 +0000

    Update isi buku

--- data.js
+++ data.js
@@ -1,9 +1,13 @@
 // Mengimpor data dari file-file yang terpisah
-import { dataKhilafah } from './data/khilafah.js';
-import { dataDemokrasi } from './data/demokrasi.js';
-import { dataKomunis } from './data/komunis.js';
-import { dataKerajaan } from './data/kerajaan.js';
+import { dataKhilafah } from './khilafah.js';
+import { dataDemokrasi } from './demokrasi.js';
+import { dataKomunis } from './komunis.js';
+import { dataKerajaan } from './kerajaan.js';
 
 // Menggabungkan semua data menjadi satu array menggunakan spread operator (...)
 export const databaseNegaraAwal = [
-    
+    ...dataKhilafah,
+    ...dataDemokrasi,
+    ...dataKomunis,
+    ...dataKerajaan
+];
